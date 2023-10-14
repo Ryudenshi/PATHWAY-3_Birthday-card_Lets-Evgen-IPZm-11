3d0696da969062cb0eee9b529a629d01b24a303d
Ryudenshi <skarydog17@gmail.com>
2023-10-14 12:08:17 +0300

    birthday card created. Ha

---
+++ app/src/main/java/com/example/pathway3_birthdaycard_letsevgenipzm_11/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pathway3_birthdaycard_letsevgenipzm_11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PATHWAY 3_Birthday card_Lets Evgen IPZm-11"
+include(":app")
+ 
